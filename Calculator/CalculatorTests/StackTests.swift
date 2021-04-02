ea49db51d5521af1636077f768b998dab22ad069
duckbok <duckbok22@gmail.com>
2021-04-02 17:05:26 +0900

    [Test] #7 StackTests 추가

--- Calculator/CalculatorTests/StackTests.swift
+++ Calculator/CalculatorTests/StackTests.swift
@@ -20,5 +20,63 @@ class StackTests: XCTestCase {
         sut = nil
         try super.tearDownWithError()
     }
-
+    
+    func test_빈_스택의_count는_0이다() {
+        XCTAssertEqual(sut.count, 0)
+    }
+    
+    func test_빈_스택의_isEmpty는_true이다() {
+        XCTAssert(sut.isEmpty)
+    }
+    
+    func test_빈_스택에서_peek을_하면_nil을_반환한다() {
+        XCTAssertEqual(sut.peek(), nil)
+    }
+    
+    func test_스택에_아이템을_push하면_내부배열에_추가된다() {
+        let item: Int = 3
+        
+        sut.push(item)
+        XCTAssertEqual(sut.peek(), item)
+        XCTAssertEqual(sut.count, 1)
+    }
+    
+    func test_비어있지_않은_스택의_isNotEmpty는_true이다() {
+        sut.push(3)
+        XCTAssert(sut.isNotEmpty)
+    }
+    
+    func test_스택에서_peek을_하면_가장_나중에_넣은_요소를_보여준다() {
+        let item1: Int = 3
+        let item2: Int = 4
+        
+        sut.push(item1)
+        sut.push(item2)
+        XCTAssertEqual(sut.peek(), item2)
+    }
+    
+    func test_빈_스택에서_pop을_하면_nil을_반환한다() {
+        XCTAssertEqual(sut.pop(), nil)
+    }
+    
+    func test_스택에서_pop을_하면_가장_나중에_넣은_요소를_뽑아준다() {
+        let item1: Int = 3
+        let item2: Int = 4
+        
+        sut.push(item1)
+        sut.push(item2)
+        XCTAssertEqual(sut.pop(), item2)
+        XCTAssertEqual(sut.peek(), item1)
+        XCTAssertEqual(sut.count, 1)
+    }
+    
+    func test_스택을_clear하면_모든_아이템을_제거한다() {
+        let item1: Int = 3
+        let item2: Int = 4
+        
+        sut.push(item1)
+        sut.push(item2)
+        sut.clear()
+        XCTAssertEqual(sut.count, 0)
+    }
 }
